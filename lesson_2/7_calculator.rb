693194370bbad8dc48a0d2c9470e7aecaa9f8df0
Brenno Kenji <brennokenji@gmail.com>
2021-02-21 10:43:59 -0800

    Calculator exercise before checking video tutorial

---
+++ lesson_2/7_calculator.rb
@@ -0,0 +1,31 @@
+# RB101 > Lesson 2 > 7. Calculator
+
+# asks for two numbers
+# asks for type of operation to perform: add, sub, mult, or div
+# displays result
+
+# Below is my attempt before watching the video:
+
+puts "Please input first number:"           # asks for 1st number
+num1 = Kernel.gets().chomp().to_i           # receives input 1
+puts "Please input second number:"          # asks for 2nd number
+num2 = Kernel.gets().chomp().to_i           # receives input 2
+puts "Please choose operation to perform: add (+), subtract (-), multiply (*), or divide (/): "
+operation = Kernel.gets().chomp()
+
+case operation
+when "add" , "addition" ,  "+" 
+   Kernel.puts( "Your result is: #{num1 + num2}") 
+when "multiply" , "multiplication" , "multi" , "*" 
+   Kernel.puts("Your result is: #{num1 * num2}")
+when "subtract" , "subtraction" ,"sub" , "-" 
+   Kernel.puts("Your result is: #{num1 - num2}")
+when "division" , "divide" , "div" , "/" 
+  if num2 == 0 
+    puts "Error: you cannot divide by zero"
+  else
+   Kernel.puts("Your result is: #{num1 / num2}")
+  end
+end
+
+
