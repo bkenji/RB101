3a778e166d1f8234cda3eff639479c1e52c15fb2
Brenno Kenji <brennokenji@gmail.com>
2021-05-04 15:35:36 -0700

    Small Problems 2

---
+++ Small_Problems/when_retire.rb
@@ -0,0 +1,14 @@
+
+
+puts "What is your age?"
+age = gets.chomp.to_i
+
+puts "At what age would you like to retire?"
+retirement_age = gets.chomp.to_i
+
+current_year = Time.now.year
+years_to_go = retirement_age - age
+retirement_year = current_year + years_to_go
+
+puts "It's #{current_year}. You will retire in #{retirement_year}."
+puts "You have only #{years_to_go} years of work to go!"
