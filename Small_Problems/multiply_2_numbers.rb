b3b82b4af4ec7c96112ed401a55aeae5196fdd55
Brenno Kenji <brennokenji@gmail.com>
2021-07-23 16:15:35 -0700

    Multiply 2 numbers Small Exercise

---
+++ Small_Problems/multiply_2_numbers.rb
@@ -0,0 +1,13 @@
+# Create a method that takes two arguments, multiplies them together, and returns the result
+# Data Structure:
+#   input: Number (Integer)
+#   output: Number (Integer)
+
+def multiply(n1, n2)
+  n1 * n2
+end
+
+
+p multiply([3, 5, 7], 2)
+p multiply("hi", 2)
+p multiply(["hello", "there"], 2)
